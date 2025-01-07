3b81b5b5cd7c3c6c815b332370e06bde1a584b64
ImHyukJin <ohj9575@gmail.com>
2025-01-07 16:16:40 +0900

    Chore: '프로젝트 생성 및 빌드수정'

---
+++ src/main/java/com/data/wowguideweb/WowguidewebApplication.java
@@ -0,0 +1,13 @@
+package com.data.wowguideweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowguidewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowguidewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/wowguideweb/WowguidewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.wowguideweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowguidewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
